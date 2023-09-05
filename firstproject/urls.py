e775925c701a7d7b4ea782ff639012b84a4c0ae9
Omar Juma <jumao2669@gmail.com>
2023-09-06 01:02:23 +0300

    Day 3: I have a basic understanding of django template engine

--- firstproject/urls.py
+++ firstproject/urls.py
@@ -16,13 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from pages.views import  home_view
-from pages.views import  about_view
-from pages.views import  contact_view
+from pages.views import home_view
+from pages.views import about_view
+from pages.views import contact_view
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', home_view, name="Home"),
+    path("admin/", admin.site.urls),
+    path("", home_view, name="Home"),
     path("home/", home_view),
-    path("about/",about_view),
-    path("contact/", contact_view)
+    path("about/", about_view),
+    path("contact/", contact_view),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
-def home_view(*args, **kwargs):
-    return HttpResponse("<h1>Homepage, Hi from Django</h1>")
-# Create your views here.
-def about_view(*args, **kwargs):
-    return HttpResponse("<h1>About page: Hello there</h1>")
-def contact_view(*args, **kwargs):
-    return HttpResponse("<h1>Contact page: Here is my contact 📞</h1>")
+def home_view(request, *args, **kwargs):
+    return render(request, "home.html", {})
+
+
+def about_view(request, *args, **kwargs):
+    return render(request, "about.html", {})
+
+
+def contact_view(request, *args, **kwargs):
+    return render(request, "contact.html", {})
